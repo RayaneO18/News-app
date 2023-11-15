d811af9bcc4a6279727bda27948bc03ab8aa47fd
Karatintseva <135255504+Karina-K-dev@users.noreply.github.com>
2023-11-15 12:42:31 +0100

    starterkit

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,9 +1,7 @@
-import "antd/dist/antd.css";
 import styles from "@/styles/Home.module.css";
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
-import { Popover, Button } from "antd";
 import News from "./News";
 
 export default function Home() {
@@ -14,23 +12,23 @@ export default function Home() {
     fetch(
       "https://newsapi.org/v2/top-headlines?country=us&apiKey=9fb8d4d606404879ab28d38b4cf2dbb2"
     )
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return res.json();
-      })
+      .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-
-        const formattedData = data.articles.map((news) => ({
-          title: news.title,
-          description: news.description,
-          urlToImage: news.urlToImage,
-          publishedAt: news.publishedAt,
-          source: news.source,
-        }));
+        const formattedData = data.articles.map((news) => {
+          return {
+            title: news.title,
+            description: news.description,
+            urlToImage: news.urlToImage,
+            publishedAt: news.publishedAt,
+            source: news.source,
+          };
+        });
         setNewsData(formattedData);
       });
   }, []);
+  const news = newsData.map((item, i) => {
+    return <News key={i} {...item} />;
+  });
+
+  return <>{news}</>;
 }
