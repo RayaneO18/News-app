967de811ccca02bf0958b5e7dac7d37d6fffe224
Karatintseva <135255504+Karina-K-dev@users.noreply.github.com>
2023-11-15 11:58:20 +0100

    api debug

--- components/Home.jsx
+++ components/Home.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 import { Popover, Button } from "antd";
-import News from "./News-app";
+import News from "./News";
 
 export default function Home() {
   const [bookmark, setBookmark] = useState([]);
@@ -14,18 +14,23 @@ export default function Home() {
     fetch(
       "https://newsapi.org/v2/top-headlines?country=us&apiKey=9fb8d4d606404879ab28d38b4cf2dbb2"
     )
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
       .then((data) => {
         console.log(data);
-        const formatedData = data.results.map((new)=> {
-          return {
-    title: new.title,
-   description: new.description,
-   urlToImage: new.urlToImage,
-   publishedAt: new.publishedAt,
-   source: new.source,
-          };
-        });
-        setNewsData(formatedData);
+
+        const formattedData = data.articles.map((news) => ({
+          title: news.title,
+          description: news.description,
+          urlToImage: news.urlToImage,
+          publishedAt: news.publishedAt,
+          source: news.source,
+        }));
+        setNewsData(formattedData);
       });
   }, []);
+}
