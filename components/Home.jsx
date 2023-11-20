1b392326b39b58368bc3300a732cfb6c346aa055
Karatintseva <135255504+Karina-K-dev@users.noreply.github.com>
2023-11-20 10:52:22 +0100

    rayan

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,68 +1,52 @@
+// Home.jsx
 import styles from "@/styles/Home.module.css";
 import React, { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBookmark } from "@fortawesome/free-solid-svg-icons";
+import Header from "./Header";
 import News from "./News";
 import MainNews from "./MainNews";
-import { Popover, Button } from "antd";
-import Link from "next/link";
 import { useSelector } from "react-redux";
 
 export default function Home() {
   const [bookmark, setBookmark] = useState([]);
   const [newsData, setNewsData] = useState([]);
   const [mainNews, setMainNews] = useState([]);
-  const bookmarks = useSelector((state) => state.bookmarks.value);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    fetch(
-      "https://newsapi.org/v2/top-headlines?country=us&apiKey=9fb8d4d606404879ab28d38b4cf2dbb2"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const formattedData = data.articles.map((news) => {
-          return {
-            title: news.title,
-            description: news.description,
-            urlToImage: news.urlToImage,
-            publishedAt: news.publishedAt,
-            source: news.source,
-          };
-        });
+    const fetchNews = async () => {
+      try {
+        const response = await fetch(
+          `https://newsapi.org/v2/top-headlines?country=us&apiKey=9fb8d4d606404879ab28d38b4cf2dbb2&q=${searchTerm}`
+        );
+        const data = await response.json();
+        const formattedData = data.articles.map((news) => ({
+          title: news.title,
+          description: news.description,
+          urlToImage: news.urlToImage,
+          publishedAt: news.publishedAt,
+          source: news.source,
+        }));
         setMainNews(formattedData.slice(0, 1));
         setNewsData(formattedData.slice(1));
-      });
-  }, []);
-  const mainNewsCard = mainNews.map((el, i) => {
-    return <MainNews key={i} {...el} />;
-  });
-  const news = newsData.map((item, i) => {
-    const isBookmarked = bookmarks.find((el) => el.id === item.id);
-    return <News isBookmarked={isBookmarked} key={i} {...item} />;
-  });
-  const popoverContent = <div>1 news like</div>;
+      } catch (error) {
+        console.error("Error fetching news:", error);
+      }
+    };
+
+    fetchNews();
+  }, [searchTerm]);
+
+  const handleSearch = (value) => {
+    setSearchTerm(value);
+  };
+
+  const mainNewsCard = mainNews.map((el, i) => <MainNews key={i} {...el} />);
+
+  const news = newsData.map((item, i) => <News key={i} {...item} />);
+
   return (
     <>
-      <div className={styles.header}>
-        <img className={styles.logo} src="logo.png" alt="" />
-        <div>
-          <Popover
-            title="Liked news"
-            content={popoverContent}
-            className={styles.popover}
-            trigger="click"
-          >
-            <Link href="/bookmarks" legacyBehavior>
-              <a>
-                <FontAwesomeIcon
-                  icon={faBookmark}
-                  className={styles.iconLink}
-                />
-              </a>
-            </Link>
-          </Popover>
-        </div>
-      </div>
+      <Header onSearch={handleSearch} />
       <div className={styles.mainNewsCard}>{mainNewsCard}</div>
       <div className={styles.newsContainer}>{news}</div>
     </>
