24e9bd5c51e0cc6a4de577309a26102a4dc061a3
Karatintseva <135255504+Karina-K-dev@users.noreply.github.com>
2023-11-20 12:12:36 +0100

    bookmarks

--- components/Bookmarks.jsx
+++ components/Bookmarks.jsx
@@ -2,24 +2,34 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
+import styles from "@/styles/Bookmarks.module.css";
+
 export default function Bookmarks() {
   const bookmarks = useSelector((state) => state.bookmarks.value);
   console.log("bkmrks", bookmarks);
-  const isBookmarked = bookmarks.find((item) => item.id === el.id);
+
   return (
     <div>
       {bookmarks.map((bookmark) => (
         <div>
-          <img src={bookmark.urlToImage} alt="Poster" />
-          <span>{bookmark.title}</span>
-          <p>{bookmark.description}</p>
-          <div>
-            <FontAwesomeIcon
-              icon={faBookmark}
-              onClick={() =>
-                isBookmarked ? handleRemoveBookmark() : handleBookmark()
-              }
+          <div className={styles.card}>
+            <img
+              className={styles.image}
+              src={bookmark.urlToImage}
+              alt="Poster"
             />
+            <div className={styles.textContainer}>
+              <span className={styles.name}>{bookmark.title}</span>
+              <p className={styles.description}>{bookmark.description}</p>
+            </div>
+            <div className={styles.iconContainer}>
+              <FontAwesomeIcon
+                icon={faBookmark}
+                onClick={() =>
+                  isBookmarked ? handleRemoveBookmark() : handleBookmark()
+                }
+              />
+            </div>
           </div>
         </div>
       ))}
